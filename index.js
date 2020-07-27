fe85808430050e48cb441a805e772e2daf0a26e9
srujansurapaneni <srujansurapaneni@gmail.com>
2020-07-27 00:37:05 -0400

    added db connections

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // define imports
 const express = require("express");
+const sql = require("mssql");
 
 // define app
 const app = express();
@@ -20,6 +21,27 @@ app.post("/", function (req, res) {
   console.log("Posted");
   // We must end the request when we are done handling it
   res.end();
+});
+
+// Database Connections
+//define config
+const config = {
+  user: "SA",
+  password: "OneforYou1",
+  server: "localhost", // You can use 'localhost\\instance' to connect to named instance
+  database: "master",
+};
+//make connection
+sql.connect(config, (err) => {
+  // Query
+  new sql.Request().query(
+    "select * from dbo.test_table tt where name = 'Dinesh';",
+    (err, result) => {
+      // ... error checks
+
+      console.dir(result);
+    }
+  );
 });
 
 // serve the app
